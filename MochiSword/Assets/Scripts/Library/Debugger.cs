81397708650237edb464dee50b362bf7b465fd91
toshi <toshi104kn@gmail.com>
2021-02-04 03:03:36 +0900

    Add: debugger

---
+++ MochiSword/Assets/Scripts/Library/Debugger.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Library {
+    public class Debugger {
+        /// <summary>
+        /// Debug.LogをUnity Editorでのみ表示する
+        /// </summary>
+        [Conditional("UNITY_EDITOR")]
+        public static void Log(object o) {
+            UnityEngine.Debug.Log(o);
+        }
+    }
+}
