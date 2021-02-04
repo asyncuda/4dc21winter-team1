9190ed07309687a29bc31be412d7667ca514e857
toshi <toshi104kn@gmail.com>
2021-02-04 19:25:55 +0900

    Fix: small

---
+++ MochiSword/Assets/Scripts/Library/Audio/SePlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Library.Audio {
+    [RequireComponent(typeof(AudioSource))]
+    public class SePlayer : MonoBehaviour {
+        private AudioSource audioSource;
+
+        private void Start() {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        /// <summary>
+        /// SEを1回再生する
+        /// </summary>
+        public void PlayOneShot(AudioClip clip) {
+            audioSource.PlayOneShot(clip);
+        }
+
+        /// <summary>
+        /// 音量を調整してSEを1回再生する
+        /// </summary>
+        public void PlayOneShot(AudioClip clip, float volumeScale) {
+            audioSource.PlayOneShot(clip, volumeScale);
+        }
+    }
+}
---
+++ MochiSword/Assets/Scripts/Library/Audio/SoundDatabase.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Library.Audio {
+    [CreateAssetMenu(menuName = "Database/SoundDatabase")]
+    public class SoundDatabase : ScriptableObject {
+        [SerializeField] private AudioClip jumpClip = default;
+
+        public AudioClip JumpClip => jumpClip;
+    }
+}
