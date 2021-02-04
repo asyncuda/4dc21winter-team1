8a4e06f9b8ea00bc5c8b011359a059c45117e134
toshi <toshi104kn@gmail.com>
2021-02-05 04:27:22 +0900

    Add: quit game when input esc

---
+++ MochiSword/Assets/Scripts/Library/ForceQuitGame.cs
@@ -0,0 +1,13 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Library {
+    public class ForceQuitGame : MonoBehaviour {
+        private void Start() {
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetKeyDown(KeyCode.Escape))
+                .Subscribe(_ => Application.Quit())
+                .AddTo(this);
+        }
+    }
+}
