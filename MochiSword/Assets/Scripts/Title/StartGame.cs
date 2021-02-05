1e338ea684b85a847e62b1bd95b08573cfac24a8
toshi <toshi104kn@gmail.com>
2021-02-06 04:10:57 +0900

    Fix: small

---
+++ MochiSword/Assets/Scripts/Title/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using Library.Scene;
+using UniRx;
+using UnityEngine;
+
+namespace Title {
+    public class StartGame : MonoBehaviour {
+        private void Start() {
+            Observable.EveryUpdate()
+                .Where(_ => Input.anyKey)
+                .Subscribe(_ => SceneMover.MoveAsync(Scenes.Game).Forget())
+                .AddTo(this);
+        }
+    }
+}
