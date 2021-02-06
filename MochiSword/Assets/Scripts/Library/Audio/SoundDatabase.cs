4f349a9af5c1741efdd63b1e317c1666808e6b79
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2021-02-06 10:08:03 +0900

    Add SE

--- MochiSword/Assets/Scripts/Library/Audio/SoundDatabase.cs
+++ MochiSword/Assets/Scripts/Library/Audio/SoundDatabase.cs
@@ -4,7 +4,16 @@ namespace Library.Audio {
     [CreateAssetMenu(menuName = "Database/SoundDatabase")]
     public class SoundDatabase : ScriptableObject {
         [SerializeField] private AudioClip jumpClip = default;
+        [SerializeField] private AudioClip killEnemyClip = default;
+        [SerializeField] private AudioClip furiClip = default;
+        [SerializeField] private AudioClip tukiClip = default;
+        [SerializeField] private AudioClip gameStartClip = default;
+        
 
         public AudioClip JumpClip => jumpClip;
+        public AudioClip KillEnemyClip => killEnemyClip;
+        public AudioClip FuriClip => furiClip;
+        public AudioClip TukiClip => tukiClip;
+        public AudioClip GameStartClip => gameStartClip;
     }
 }
