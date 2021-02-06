62d57432596cfaa90b86cbfb2e56bbcc30ce5034
Guppy-ZEN <39458650+g-zen@users.noreply.github.com>
2021-02-06 09:50:43 +0900

    ボス部屋への移動を追加

---
+++ MochiSword/Assets/Scripts/MoveBossScene.cs
@@ -0,0 +1,13 @@
+﻿using Library.Scene;
+using UnityEngine;
+
+public class MoveBossScene : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneMover.MoveAsync(Scenes.Boss).Forget();
+        }
+    }
+}
