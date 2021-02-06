cd7c4eddbc09b0d2974dca2cf933ec385d6a28eb
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2021-02-06 12:28:45 +0900

    SE 修正

--- MochiSword/Assets/Scripts/Enemy/Boss/SoftBossHealth.cs
+++ MochiSword/Assets/Scripts/Enemy/Boss/SoftBossHealth.cs
@@ -1,6 +1,8 @@
 ﻿using Library.Scene;
 using UniRx;
 using UnityEngine;
+using Zenject;
+using Library.Audio;
 
 namespace Enemy.Boss {
     /// <summary>
@@ -10,10 +12,14 @@ namespace Enemy.Boss {
     public class SoftBossHealth : MonoBehaviour, IReceivableStab {
         [SerializeField] private int health = default;
 
+        [Inject] SePlayer sePlayer;
+        [Inject] SoundDatabase soundDatabase;
+
         private void Start() {
             this.ObserveEveryValueChanged(x => x.health)
                 .Where(x => x <= 0)
                 .Subscribe(_ => {
+                    sePlayer.PlayOneShot(soundDatabase.DefeatClip);
                     gameObject.SetActive(false);
                     SceneMover.MoveAsync(Scenes.Result).Forget();
                 })
